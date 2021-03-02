53493eeb14aec28bf5246b88f4bd17bafa6db410
velllum <velllum@yandex.ru>
2021-03-03 00:15:42 +0300

    Uploads files

--- parse.py
+++ parse.py
@@ -1,5 +1,11 @@
+import csv
+import os
+import sys
+
 import requests
 from bs4 import BeautifulSoup
+
+base_path = os.path.abspath(os.path.dirname(__file__))
 
 headers = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36"
@@ -8,88 +14,9 @@ headers = {
 session = requests.Session()
 
 
-def get_response(url):
-    response = session.get(url, headers=headers)
+def get_response(url, stream=False):
+    response = session.get(url, headers=headers, stream=stream)
     return response
-
-
-def recursion(lis, breadcrumb):
-    # submenu = li.find_all("li")
-
-    lis = []
-
-    # print("ppp", (lis.a.get("href"), breadcrumb))
-
-    for li in lis:
-
-        print("*" * 100)
-
-        breadcrumbs = f"{breadcrumb} > {str(li.text).encode('l1').decode()}"
-        link = li.find("a").get("href")
-
-        print(link, breadcrumbs)
-
-        # lis.append((link, breadcrumbs))
-
-        print("*" * 100)
-
-        if "dropdown" in li.get("class"):
-            list_data = recursion(li, breadcrumbs)
-            # lis.extend(list_data)
-
-    # return lis
-
-
-def obtain_nesting(response):
-    soup = BeautifulSoup(response.text, "lxml")
-    menu = soup.find("ul", {"id": "menu-katalog"}).find_all("li", {"class": "menu-item"})
-
-    lis = []
-
-    for li in menu:
-
-        if "dropdown" in li.get("class"):
-
-            print("_" * 100)
-
-            breadcrumbs = "Главная"
-
-            breadcrumbs += f" > {str(li.text).encode('l1').decode()}"
-            link = li.a.get("href")
-
-            lis.append((link, breadcrumbs))
-
-            # print(link, breadcrumbs)
-
-            recursion(li, breadcrumbs)
-
-            print("_" * 100)
-
-        else:
-
-            breadcrumbs = "Главная"
-
-            breadcrumbs += f" > {str(li.text).encode('l1').decode()}"
-            link = li.a.get("href")
-
-            print(link, breadcrumbs)
-
-            print("=" * 100)
-
-    #         list_links.append((link, breadcrumbs))
-    #
-    # print(list_links)
-
-
-def get_links(response):
-    soup = BeautifulSoup(response.text, "lxml")
-    links = soup.find("ul", {"id": "menu-katalog"}).find_all("a")
-    list_links = []
-    for e, link in enumerate(links, 1):
-        print(e, link.get("href"), str(link.text).encode('l1').decode())
-        list_links.append((link.get("href"), str(link.text).encode('l1').decode()))
-
-    return list_links
 
 
 def collect_data(link):
@@ -111,73 +38,124 @@ def collect_data(link):
     return urls_and_images
 
 
+def save_data_csv_files(list_dicts):
+    """Сохранение данных в файл CSV"""
+    with open('products_data.csv', 'a', encoding='utf-8', newline='') as csv_file:
+        list_key = list_dicts[0].keys()
+        print(list_key)
+        dict_writer = csv.DictWriter(csv_file, list_key)
+        dict_writer.writeheader()
+        dict_writer.writerows(list_dicts)
+
+
+def save_images(urls_images):
+    """сохранение файла, по пути указанной в ссылке"""
+    list_images = [im for _, images in urls_images for im in images]
+    for image in list_images:
+        list_element = str(image).rsplit("/", 1)
+
+        file_name = list_element[-1]
+        path_files = list_element[0].split("/", 4)[-1]
+
+        full_path_files = os.path.join(base_path, path_files)
+
+        try:
+            os.makedirs(full_path_files)
+        except OSError:
+            pass
+
+        print(list_element, path_files, file_name)
+
+        response = get_response(image, stream=True)
+        with open(os.path.join(full_path_files, file_name), 'wb') as fd:
+            for chunk in response.iter_content(chunk_size=1024):
+                fd.write(chunk)
+
+
 def collect_page_data(urls_images):
-    """Собрать данные с страницы str(link.text).encode('l1').decode()"""
-    # for url_image in urls_images:
-    response = get_response("https://www.telemagazin.by/product/urna-s-ershikom/")
-    # response = get_response(url_image[0])
-    soup = BeautifulSoup(response.text, "lxml")
+    """Собрать данные с страницы"""
+    list_dicts = []
+    for enum, url_image in enumerate(urls_images):
+        response = get_response(url_image[0])
+        soup = BeautifulSoup(response.text, "lxml")
 
+        try:
+            number = soup.find("div", {"itemprop": "offers"}).find("p", {"class": "price"})
 
-    try:
-        number = soup.find("div", {"itemprop": "offers"}).find("p", {"class": "price"})
+            if number.ins:
+                number = number.ins.text.split()[0]
+            else:
+                number = number.text.split()[0]
 
-        if number.ins:
-            number = number.ins.text.split()[0]
-        else:
-            number = number.text.split()[0]
+            price = number
+        except:
+            price = ""
 
-        price = number
-    except:
-        price = ""
+        try:
+            number = soup.find("div", {"itemprop": "offers"}).find("p", {"class": "price"})
 
+            crossed_price = number.find("del").text.split()[0]
+        except:
+            crossed_price = ""
 
-    try:
-        number = soup.find("div", {"itemprop": "offers"}).find("p", {"class": "price"})
+        try:
+            stock = soup.find("p", {"class": "stock out-of-stock"})
+            stock = str(stock.span.text).encode('l1').decode()
+        except:
+            stock = ""
 
-        crossed_price = number.find("del").text.split()[0]
-    except:
-        crossed_price = ""
+        try:
+            description = soup.find("div", {"class": "woo-short-description"})
+            description = str(description).encode('l1').decode()
+        except:
+            description = ""
 
+        try:
+            content = soup.find("div", {"id": "tab-description"})
+            if content:
+                content = str(content).encode('l1').decode()
+            else:
+                content = ""
+        except:
+            content = ""
 
-    try:
-        stock = soup.find("p", {"class": "stock out-of-stock"})
-        stock = str(stock.span.text).encode('l1').decode()
-    except:
-        stock = ""
+        try:
+            title = soup.find("h1")
+            title = str(title.text).encode('l1').decode()
+        except:
+            title = ""
 
+        try:
+            category = soup.find("div", {"class": "crumb-flex"}).find_all("a")
+            links = [crumb.get("href") for crumb in category]
+            list_breadcrumbs = []
 
-    try:
-        description = soup.find("div", {"class": "woo-short-description"})
-        description = str(description).encode('l1').decode()
-    except:
-        description = ""
+            for link in links:
+                response = get_response(link)
+                soup = BeautifulSoup(response.text, "lxml")
+                breadcrumbs = soup.find("nav", {"class": "breadcrumbs"}).find_all("span", {"itemprop": "title"})
+                list_breadcrumbs.append(
+                    " > ".join([str(breadcrumb.text).encode('l1').decode() for breadcrumb in breadcrumbs]))
+            categories = ", ".join(list_breadcrumbs)
+        except:
+            categories = ""
 
+        data_dict = dict(
+            price=price,
+            crossed_price=crossed_price,
+            stock=stock,
+            description=description,
+            content=content,
+            title=title,
+            categories=categories,
+            image=", ".join(url_image[1]),
+        )
 
-    try:
-        content = soup.find("div", {"id": "tab-description"})
-        content = str(content).encode('l1').decode()
-    except:
-        content = ""
+        print(enum, url_image[0], data_dict)
 
+        list_dicts.append(data_dict)
 
-    try:
-        title = soup.find("h1")
-        title = str(title.text).encode('l1').decode()
-    except:
-        title = ""
-
-
-    try:
-        crumb = soup.find("div", {"class": "crumb-flex"}).find_all("a")
-        print(crumb)
-    except:
-        title = ""
-
-
-    # print(url_image[0], url_image[1], price, stock, description, content, title)
-    # print(price, crossed_price, stock, description, content, title)
-
+    return list_dicts
 
 
 
@@ -187,8 +165,9 @@ def main():
 
     for link in sitemap_links:
         collection_urls_and_images = collect_data(link)
-        collect_page_data(collection_urls_and_images)
-        break
+        save_images(collection_urls_and_images)
+        list_dicts = collect_page_data(collection_urls_and_images)
+        save_data_csv_files(list_dicts)
 
 
 if __name__ == '__main__':
