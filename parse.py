541809c419bf9dce487ff516585c8758d0d067bd
velllum <velllum@ya.ru>
2021-03-02 16:50:05 +0300

    upload collect data in csv

--- parse.py
+++ parse.py
@@ -5,73 +5,80 @@ headers = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36"
 }
 
+session = requests.Session()
+
 
 def get_response(url):
-    response = requests.get(url, headers=headers)
+    response = session.get(url, headers=headers)
     return response
 
 
-def recursion(li):
-    submenu = li.find("ul", {"class": "dropdown-submenu"}).find_all("li", {"class": "menu-item"})
+def recursion(lis, breadcrumb):
+    # submenu = li.find_all("li")
 
-    breadcrumbs = []
+    lis = []
 
-    for li in submenu:
+    # print("ppp", (lis.a.get("href"), breadcrumb))
 
-        if not li.ul:
-            breadcrumb = str(li.text).encode('l1').decode()
+    for li in lis:
 
-            breadcrumbs.append(breadcrumb)
+        print("*" * 100)
 
-            return li.a.get("href"), " > ".join(breadcrumbs)
+        breadcrumbs = f"{breadcrumb} > {str(li.text).encode('l1').decode()}"
+        link = li.find("a").get("href")
 
-        else:
-            # breadcrumbs.append(recursion(li))
-            recursion(li)
+        print(link, breadcrumbs)
 
-    # print(breadcrumbs)
-    # return link, breadcrumbs
+        # lis.append((link, breadcrumbs))
+
+        print("*" * 100)
+
+        if "dropdown" in li.get("class"):
+            list_data = recursion(li, breadcrumbs)
+            # lis.extend(list_data)
+
+    # return lis
 
 
 def obtain_nesting(response):
     soup = BeautifulSoup(response.text, "lxml")
     menu = soup.find("ul", {"id": "menu-katalog"}).find_all("li", {"class": "menu-item"})
 
-    list_links = []
-
-    breadcrumbs = "Главная"
+    lis = []
 
     for li in menu:
 
-        if li.ul:
-            # link, breadcrumb = recursion(li)
-            # breadcrumbs.append(breadcrumb)
-            #
-            # list_links.append((link, " > ".join(breadcrumbs)))
-            # list_links.append((li.a.get("href"), " > ".join(breadcrumbs)))
+        if "dropdown" in li.get("class"):
 
-            # print({str(li.ul.text).encode('l1').decode()})
-            print("_"*100)
-            #
-            # text = f"{text} > {str(li.text).encode('l1').decode()}"
-            # obtain_nesting(response, text)
+            print("_" * 100)
+
+            breadcrumbs = "Главная"
+
+            breadcrumbs += f" > {str(li.text).encode('l1').decode()}"
+            link = li.a.get("href")
+
+            lis.append((link, breadcrumbs))
+
+            # print(link, breadcrumbs)
+
+            recursion(li, breadcrumbs)
+
+            print("_" * 100)
 
         else:
 
             breadcrumbs = "Главная"
-
-            # breadcrumb = str(li.text).encode('l1').decode()
-            #
-            # breadcrumbs.append(breadcrumb)
 
             breadcrumbs += f" > {str(li.text).encode('l1').decode()}"
             link = li.a.get("href")
 
             print(link, breadcrumbs)
 
-            list_links.append((link, breadcrumbs))
+            print("=" * 100)
 
-    print(list_links)
+    #         list_links.append((link, breadcrumbs))
+    #
+    # print(list_links)
 
 
 def get_links(response):
@@ -85,16 +92,103 @@ def get_links(response):
     return list_links
 
 
+def collect_data(link):
+    """Собрать данные с карты сайта о товаре"""
+    response = get_response(link)
+
+    soup = BeautifulSoup(response.text, "lxml")
+    urls_and_images = []
+
+    for url in soup.find_all("url"):
+
+        if not url.find("image:loc"):
+            continue
+
+        links_images = [image.text for image in url.find_all("image:loc")]
+        url_and_image = url.loc.text, links_images
+        urls_and_images.append(url_and_image)
+
+    return urls_and_images
+
+
+def collect_page_data(urls_images):
+    """Собрать данные с страницы str(link.text).encode('l1').decode()"""
+    # for url_image in urls_images:
+    response = get_response("https://www.telemagazin.by/product/urna-s-ershikom/")
+    # response = get_response(url_image[0])
+    soup = BeautifulSoup(response.text, "lxml")
+
+
+    try:
+        number = soup.find("div", {"itemprop": "offers"}).find("p", {"class": "price"})
+
+        if number.ins:
+            number = number.ins.text.split()[0]
+        else:
+            number = number.text.split()[0]
+
+        price = number
+    except:
+        price = ""
+
+
+    try:
+        number = soup.find("div", {"itemprop": "offers"}).find("p", {"class": "price"})
+
+        crossed_price = number.find("del").text.split()[0]
+    except:
+        crossed_price = ""
+
+
+    try:
+        stock = soup.find("p", {"class": "stock out-of-stock"})
+        stock = str(stock.span.text).encode('l1').decode()
+    except:
+        stock = ""
+
+
+    try:
+        description = soup.find("div", {"class": "woo-short-description"})
+        description = str(description).encode('l1').decode()
+    except:
+        description = ""
+
+
+    try:
+        content = soup.find("div", {"id": "tab-description"})
+        content = str(content).encode('l1').decode()
+    except:
+        content = ""
+
+
+    try:
+        title = soup.find("h1")
+        title = str(title.text).encode('l1').decode()
+    except:
+        title = ""
+
+
+    try:
+        crumb = soup.find("div", {"class": "crumb-flex"}).find_all("a")
+        print(crumb)
+    except:
+        title = ""
+
+
+    # print(url_image[0], url_image[1], price, stock, description, content, title)
+    # print(price, crossed_price, stock, description, content, title)
+
+
+
+
 def main():
     url = "https://www.telemagazin.by/"
+    sitemap_links = [f"{url}product-sitemap1.xml", f"{url}product-sitemap2.xml"]
 
-    response = get_response(url)
-
-    # list_links = get_links(response)
-
-    # print(list_links)
-
-    obtain_nesting(response)
+    for link in sitemap_links:
+        collection_urls_and_images = collect_data(link)
+        collect_page_data(collection_urls_and_images)
+        break
 
 
 if __name__ == '__main__':
